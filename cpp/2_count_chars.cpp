3f8d47b8b93b68465a37263b951433bdfcce0383
Robert <dev@robertsimoes.org>
2021-05-26 20:35:47 -0700

    rename and adds count chars

---
+++ cpp/2_count_chars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	 string str;
+
+	 cout << "What is the input string? ";
+	 cin >> str;
+	 cout << str << " has " << str.length() << " characters";
+
+	 return 0;
+}
