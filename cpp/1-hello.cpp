7842d5cc6b22dd09ccdcf4e4b4ef6cd50dffc53a
Robert <dev@robertsimoes.org>
2021-05-26 20:22:18 -0700

    Hello world c++

---
+++ cpp/1-hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+string name;
+
+int main() {
+
+	 cout << "What is your name? ";
+	 cin >> name;
+	 cout << "Hello, " << name << ", nice to meet you!";
+
+	 return 0;
+}
+
