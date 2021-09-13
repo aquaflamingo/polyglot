58102b60fd769cb8f87a07ccab9ee093303d25eb
Robert <16901597+aquaflamingo@users.noreply.github.com>
2021-09-12 18:16:23 -0700

    Adds Rust

---
+++ rust/src/1_hello.rs
@@ -0,0 +1,11 @@
+use std::io::{self, Write, Read}
+
+fn main() {
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+
+    stdin.read_to_string(&mut bufffer)?;
+
+    // TODO
+
+}
