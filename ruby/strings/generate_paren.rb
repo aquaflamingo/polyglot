73cd69b1af3885e00bea7bf36ac9a1e5f5724719
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-11-02 18:14:00 -0400

    Adds generate paren

---
+++ ruby/strings/generate_paren.rb
@@ -0,0 +1,18 @@
+# Tag:
+# - EASY 
+#
+# Problem :
+# - Given n pairs of parenthesis write a function that computes all valid permutations of parenthesis
+# - n is smaller than 9
+# - Example
+#     Input: n = 3
+#     Output: ["((()))","(()())","(())()","()(())","()()()"]
+#
+# Assumptions:
+# - Return empty if n is zero
+# 
+# Strategy:
+# - TODO
+#
+# Complexity: 
+# - TODO
